710514c88bdb3815ff4531304117e5e5c524a2e2
Bruno Justo <85250651+BrunoJusto2002@users.noreply.github.com>
2023-04-20 17:49:57 +0000

    Home page

--- Index.js
+++ Index.js
@@ -1,3 +1,5 @@
+/* Ver com o sor pq o formulario não funcionou e as estrelas não está pintando */
+
 //* Star functionality */
 var stars = document.querySelectorAll('.star-icon');
                   
@@ -11,3 +13,14 @@ document.addEventListener('click', function(e){
     console.log(e.target.getAttribute('data-avaliacao'));
   }
 });
+
+/* Submit function */
+function Submit(event) {
+  form.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+  event.preventDefault();
+}
+
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const oponiao = document.getElementById('oponiao');
+form.addEventListener('submit', 'name',  Submit); 
