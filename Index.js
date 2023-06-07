307c90876fac0a451d5c3dad464917a2c5575299
Bruno Justo <85250651+BrunoJusto2002@users.noreply.github.com>
2023-06-07 03:18:24 +0000

    Criação do menu responsivo

--- Index.js
+++ Index.js
@@ -52,3 +52,13 @@ controls.forEach((control) => {
     items[currentItem].classList.add("current-item");
   });
 });
+
+// Creating functionality for the responsive menu
+function openNav(){
+  document.getElementById("myNav").style.width = "60%";
+}
+
+function closeNav(){
+  document.getElementById("myNav").style.width = "0";
+}
+
